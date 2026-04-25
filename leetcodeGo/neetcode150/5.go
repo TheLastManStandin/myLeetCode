8ad181cf5961ed65524321408d09fea0ef48f33d
cahisa <cahisabeat@gmail.com>
2026-04-25 15:53:29 +0300

    one task

--- leetcodeGo/neetcode150/5.go
+++ leetcodeGo/neetcode150/5.go
@@ -1,5 +1,30 @@
 package main
 
-func topKFrequent(nums []int, k int) []int {
+import "sort"
 
+func topKFrequent(nums []int, k int) []int {
+	hash := map[int]int{}
+	sliseMap := [][]int{}
+	ans := []int{}
+	for _, num := range nums {
+		_, ok := hash[num]
+		if ok {
+			hash[num] = hash[num] + 1
+		} else {
+			hash[num] = 1
+		}
+	}
+
+	for key, element := range hash {
+		sliseMap = append(sliseMap, []int{key, element})
+	}
+
+	sort.Slice(sliseMap, func(i, j int) bool {
+		return sliseMap[i][1] > sliseMap[j][1]
+	})
+
+	for i := 0; i < k; i++ {
+		ans = append(ans, sliseMap[i][0])
+	}
+	return ans
 }
