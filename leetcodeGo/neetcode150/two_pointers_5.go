cdbdb5a750add45f2c4c3ca73da8b863f2baeb7e
cahisa <cahisabeat@gmail.com>
2026-05-26 18:30:23 +0300

    try again

--- leetcodeGo/neetcode150/two_pointers_5.go
+++ leetcodeGo/neetcode150/two_pointers_5.go
@@ -7,13 +7,17 @@ func trap(height []int) int {
 	stackRight := make([]int, 0, len(height))
 	ans := 0
 
+	if len(height) < 3 {
+		return ans
+	}
+
 	for left < right {
 		if len(stackLeft) == 0 {
 			stackLeft = append(stackLeft, height[left])
 		} else {
 			if height[left] > stackLeft[0] {
 				stackLeftLen := len(stackLeft)
-				for i := 1; i < stackLeftLen-1; i++ {
+				for i := 1; i <= stackLeftLen-1; i++ {
 					ans += stackLeft[0] - stackLeft[i]
 				}
 				stackLeft = make([]int, 0, len(height))
@@ -28,7 +32,7 @@ func trap(height []int) int {
 		} else {
 			if height[right] > stackRight[0] {
 				stackRightLen := len(stackRight)
-				for i := 1; i < stackRightLen-1; i++ {
+				for i := 1; i <= stackRightLen-1; i++ {
 					ans += stackRight[0] - stackRight[i]
 				}
 				stackRight = make([]int, 0, len(height))
@@ -42,19 +46,23 @@ func trap(height []int) int {
 		right--
 	}
 
-	if left == right {
+	curLevel := min(stackRight[0], stackLeft[0])
+	if left == right && height[left] <= curLevel {
 		stackRight = append(stackRight, height[left])
 	}
-	curLevel := min(stackRight[0], stackLeft[0])
 
 	stackRightLen := len(stackRight)
-	for i := 1; i < stackRightLen-1; i++ {
-		ans += curLevel - stackRight[i]
+	for i := 1; i <= stackRightLen-1; i++ {
+		if curLevel-stackRight[i] >= 0 {
+			ans += curLevel - stackRight[i]
+		}
 	}
 
 	stackLeftLen := len(stackLeft)
-	for i := 1; i < stackLeftLen-1; i++ {
-		ans += stackLeft[0] - stackLeft[i]
+	for i := 1; i <= stackLeftLen-1; i++ {
+		if curLevel-stackLeft[i] >= 0 {
+			ans += curLevel - stackLeft[i]
+		}
 	}
 
 	return ans
